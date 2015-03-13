a784214fe641861b30ffe4c64f92b168fc163f86
Bernardo <Bernardo@bernardo>
2015-03-12 21:38:47 -0300

    Segundo Commit

--- GitDemo/src/br/com/bernardo/git/Main.java
+++ GitDemo/src/br/com/bernardo/git/Main.java
@@ -3,7 +3,7 @@ package br.com.bernardo.git;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Primeiro commit no git"); 
+		System.out.println("Segundo commit no git");  
 
 	}
 
